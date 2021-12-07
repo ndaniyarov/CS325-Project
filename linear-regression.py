6dddc03d9bbe132004c17a5e2a89a8d07803701a
Coufal Sarah <sarah.coufal1@gmail.com>
2021-12-07 17:22:50 -0500

    read file

--- linear-regression.py
+++ linear-regression.py
@@ -1,1 +1,6 @@
 # linear regression
+
+from read_file import *
+
+(x_train, y_train) = read_csv()
+print(y_train)
---
+++ read_file.py
@@ -0,0 +1,45 @@
+import csv
+import os.path
+import sys
+import numpy as np
+
+
+def read_csv():
+    # open and read file
+    if os.path.exists('music-data.csv'):
+        csvfile = open('music-data.csv')
+        readCSV = csv.reader(csvfile, delimiter=',')
+    else:
+        print("File not found.")
+        sys.exit()
+
+    # skip first line
+    next(readCSV)
+
+    i = 0
+
+    name = 2
+    date = 3
+    genre = 4
+    lyrics = 5
+
+    all_lyrics = []
+    all_dates = []
+
+    for row in readCSV:
+
+        all_lyrics.append(row[lyrics])
+        all_dates.append(int(row[date]))
+
+        # print(row[lyrics])
+        i += 1
+        if i > 10:
+            break
+
+    # convert to numpy arrays
+    x_lyrics = np.array(all_lyrics)
+    y_dates = np.array(all_dates)
+    return (x_lyrics, y_dates)
+
+
+# print(x_lyrics)
